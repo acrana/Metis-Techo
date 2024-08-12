3d9795385f9b7bda34827e669f3ba86b1229a448
acrana <acrana12@gmail.com>
2024-08-11 20:05:19 -0500

    Update train_model.py

--- models/train_model.py
+++ models/train_model.py
@@ -1,6 +1,5 @@
 # src/train_model.py
 import pandas as pd
-import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
