91fcb8d08a2b629dc014b19e4ecad456d021e916
acrana <acrana12@gmail.com>
2024-09-22 13:58:55 -0500

    Update data_access.py

--- src/data_access.py
+++ src/data_access.py
@@ -26,3 +26,12 @@ def get_patient_ade_records(patient_id):
     conn.close()
     return df
 
+def get_patient_ade_records(patient_id):
+    db_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'clinical_decision_support.db')
+    conn = sqlite3.connect(db_path)
+    query = f"SELECT * FROM TBL_ADERecords WHERE PatientID = {patient_id}"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
+
+
