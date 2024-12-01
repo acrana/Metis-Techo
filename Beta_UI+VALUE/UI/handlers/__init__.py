415b1673e86a01f53305da94220250a05d44cacf
acrana <acrana12@gmail.com>
2024-12-01 06:27:38 -0600

    Delete Beta_UI+VALUE directory

--- Beta_UI+VALUE/UI/handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-# In TESTING/UI/__init__.py
-# Leave completely empty, just save the file
-
-# In TESTING/UI/main_window/__init__.py
-# Leave completely empty, just save the file
-
-# In TESTING/UI/tabs/__init__.py
-# Leave completely empty, just save the file
